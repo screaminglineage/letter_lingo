17930e236b95e1d6f9751e068c8f727c2fd21b6a
Aditya Ghosh <adityaghosh5025@gmail.com>
2022-06-19 01:51:43 +0530

    Added randomly generated words

--- src/handlers/word_handler.rs
+++ src/handlers/word_handler.rs
@@ -1,88 +1,22 @@
-// Functions to handle checking user input with answer
+// Functions to get a list of words and randomly choose one
 
-use crate::config::*;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use rand::Rng;
 
-// Converts word into an char vector
-fn get_letters(word: &String) -> Vec<char> {
-    let letters: Vec<char> = word.chars().collect();
-    letters
+
+pub fn get_word_file(file: &str) -> Vec<String> {
+    let file =  File::open(file).expect("File Not Found");
+    let buffer = BufReader::new(file);
+    buffer
+        .lines()
+        .map(|l| l.expect("Failed to Parse Line"))
+        .collect()
 }
 
-/*  Compares answer and a word and returns a char vector containing
-    - g -> letter in answer and correct position
-    - y -> letter in answer but wrong position
-    - w -> letter not in answer
-*/
-pub fn check_word(answer: &String, word: &String) -> Vec<char> {
-    let mut ans_letters = get_letters(answer);
-    let mut word_letters = get_letters(word);
-    let mut check_vec: Vec<char> = Vec::new();
 
-    // Marks the letters (green, yellow, white) in a vector
-    // and returns the vector
-    mark_correct(&mut ans_letters, &mut word_letters, &mut check_vec);
-    mark_incorrect(&mut ans_letters, &mut word_letters, &mut check_vec);
-    check_vec
-}
-
-/*  Checks for characters which are in the correct
-    position and removes them after marking the position
-    as correct in the check_vec vector
-*/
-pub fn mark_correct(
-    ans_letters: &mut Vec<char>,
-    word_letters: &mut Vec<char>,
-    check_vec: &mut Vec<char>,
-) {
-    let length = WORD_LENGTH;
-    let mut check: char;
-    for i in 0..length {
-        if ans_letters[i] == word_letters[i] {
-            ans_letters[i] = ' ';
-            word_letters[i] = ' ';
-            check = 'g';
-        } else {
-            check = ' ';
-        }
-        check_vec.push(check);
-    }
-}
-
-/*  Checks for every character in the input word which
-    also present in the answer and removes them both after
-    marking the position as wrong position but correct letter
-    in the check_vec vector
-*/
-pub fn mark_incorrect(
-    ans_letters: &mut Vec<char>,
-    word_letters: &mut Vec<char>,
-    check_vec: &mut Vec<char>,
-) {
-    let length = WORD_LENGTH;
-
-    for i in 0..length {
-        let mut flag = false;
-
-        if check_vec[i] == ' ' {
-            for _ in 0..length {
-                let letter = word_letters[i];
-                if ans_letters.contains(&letter) {
-                    // Gets position of the letter in the answer
-                    let pos = ans_letters
-                        .iter()
-                        .position(|&x| x == letter)
-                        .expect("Failed to find letter in answer");
-
-                    flag = true;
-                    check_vec[i] = 'y';
-                    word_letters[i] = ' ';
-                    ans_letters[pos] = ' ';
-                    break;
-                }
-            }
-            if flag == false {
-                check_vec[i] = 'w';
-            }
-        }
-    }
+pub fn get_word(words: &Vec<String>) -> &String {
+    let mut rng = rand::thread_rng();
+    let i = rng.gen_range(0..words.len());
+    &words[i]
 }
